1938812fc48c3d31520f26d47e7b614bc544635b
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-12-11 10:28:05 +0100

    sandbox - add placeholder text

--- src/pages/Sandbox.jsx
+++ src/pages/Sandbox.jsx
@@ -1,8 +1,8 @@
 function Sandbox() {
   return (
-    <>
-    
-    </>
+    <div className="container text-center py-32 px-2 text-xl">
+      This <span className="text-port-yellow">sandbox</span> is so fresh, even the grains of sand have not arrived yet.
+    </div>
   )
 }
 
