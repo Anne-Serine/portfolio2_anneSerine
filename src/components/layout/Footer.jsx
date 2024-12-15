5608712dfc0ec8eaf4473bc62e1ed0ee36b1de6f
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-12-15 20:51:30 +0100

    adjust menue btn, hero img, footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,9 +1,11 @@
 function Footer() {
   return (
-    <div className="text-center mt-20 mb-5 text-xs">
-          <p>2024 &copy; Anne-Serine Johannessen</p>
-        </div>
-  )
+    <footer className="footer">
+      <div className="text-center mt-20 mb-5 text-xs">
+        <p>2024 &copy; Anne-Serine Johannessen</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
