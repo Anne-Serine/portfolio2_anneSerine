42cc512a853033bf273de84a12240a0627d88c32
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-12-15 19:09:57 +0100

    update color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ export default {
         "port-bg": "#131514",
         "port-dark": "#090B0A",
         "port-yellow": "#FFC86A",
+        "port-gray": "#242524",
         "port-light-gray": "#F3F3F3",
         "port-white": "#F9F9F9"
       },
