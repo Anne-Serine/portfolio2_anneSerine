47f5fbe34ff0e82ef7074bc696cf465a5764a1e5
Anne-Serine <125912893+Anne-Serine@users.noreply.github.com>
2024-12-15 19:11:56 +0100

    timeline - create component

---
+++ src/components/features/Timeline.jsx
@@ -0,0 +1,31 @@
+function Timeline() {
+  return (
+    <ol className="timeline py-2">
+      <li>
+        <div>2025</div>
+        <div className="flowstep"></div>
+        <div className="answerstep"></div>
+        <div className="flownext"></div>
+        <div>
+          <p> Web developer at Appex </p>
+        </div>
+      </li>
+      <li>
+        <div>2024</div> <div className="flowstep"></div>
+        <div className="answerstep"></div>
+        <div className="flownext"></div>
+        <div>
+          <p> Second year at Noroff School of Technology and Digital Media </p>
+        </div>
+      </li>
+      <li>
+        <div>2023</div> <div className="flowstep"></div>
+        <div className="answerstep"></div>
+        <div>
+          <p> First year at Noroff School of Technology and Digital Media </p>
+        </div>
+      </li>
+    </ol>
+  );
+}
+export default Timeline;
